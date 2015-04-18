4678ac443bf4cf58fdce6c1bb25679b4ba64bb60
Larry Jones <mrwizard82d1@gmail.com>
2015-04-18 16:04:46 -0500

    Refactor URI source into context.

--- test/lorem_ipsum_test.exs
+++ test/lorem_ipsum_test.exs
@@ -1,20 +1,22 @@
 defmodule LoremIpsumTest do
   use ExUnit.Case
 
+	setup do
+		{:ok, [uri_source: "http://www.loremipsum.com"]}
+	end
+
 	test "construct from string" do
 		source = "Lorem ipsum dolor sit"
 		sut = LoremIpsum.new source
 		assert (LoremIpsum.source sut) == source
 	end
 
-	test "construct from uri has correct scheme" do
-		source = "http://www.loremipsum.com"
+	test "construct from uri has correct scheme", %{uri_source: source} do
 		sut = LoremIpsum.new source
 		assert (LoremIpsum.source sut).scheme == URI.parse(source).scheme
 	end
 
-	test "construct from uri has correct host" do
-		source = "http://www.loremipsum.com"
+	test "construct from uri has correct host", %{uri_source: source} do
 		sut = LoremIpsum.new source
 		assert (LoremIpsum.source sut).host == URI.parse(source).host
 	end
