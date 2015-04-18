9ed97e605b2ef1d367dc13ba346ca88d1be034b0
Larry Jones <mrwizard82d1@gmail.com>
2015-04-18 15:09:18 -0500

    Write failing constructor test.

--- lib/lorem_ipsum.ex
+++ lib/lorem_ipsum.ex
@@ -1,2 +1,7 @@
 defmodule LoremIpsum do
+	def new(source) do
+	end
+	
+	def source(lorem_ipsum) do
+	end
 end
--- test/lorem_ipsum_test.exs
+++ test/lorem_ipsum_test.exs
@@ -1,7 +1,10 @@
 defmodule LoremIpsumTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
+	test "construct from string" do
+		source = "Lorem ipsum dolor sit"
+		sut = LoremIpsum.new source
+		assert (LoremIpsum.source sut) == source
+	end
+
 end
