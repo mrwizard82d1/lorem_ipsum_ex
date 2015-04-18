38ee29e0efdedc4cc10ac09835563e1820f796b0
Larry Jones <mrwizard82d1@gmail.com>
2015-04-18 16:08:05 -0500

    Refactor creating SUT into common method.

--- test/lorem_ipsum_test.exs
+++ test/lorem_ipsum_test.exs
@@ -7,18 +7,22 @@ defmodule LoremIpsumTest do
 
 	test "construct from string" do
 		source = "Lorem ipsum dolor sit"
-		sut = LoremIpsum.new source
+		sut = new_sut source
 		assert (LoremIpsum.source sut) == source
 	end
 
 	test "construct from uri has correct scheme", %{uri_source: source} do
-		sut = LoremIpsum.new source
+		sut = new_sut source
 		assert (LoremIpsum.source sut).scheme == URI.parse(source).scheme
 	end
 
 	test "construct from uri has correct host", %{uri_source: source} do
-		sut = LoremIpsum.new source
+		sut = new_sut source
 		assert (LoremIpsum.source sut).host == URI.parse(source).host
+	end
+
+	def new_sut(source) do
+		LoremIpsum.new source
 	end
 
 end
